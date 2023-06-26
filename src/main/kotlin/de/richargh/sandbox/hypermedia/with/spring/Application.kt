4f453c29fd471a54dd6eaf8f8cb0fa1bd32bb4c0
Richard Gross <richard.gross@maibornwolff.de>
2023-06-26 16:59:34 +0200

    r move bean definition to RootBeans file

--- src/main/kotlin/de/richargh/sandbox/hypermedia/with/spring/Application.kt
+++ src/main/kotlin/de/richargh/sandbox/hypermedia/with/spring/Application.kt
@@ -2,25 +2,10 @@ package de.richargh.sandbox.hypermedia.with.spring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.hateoas.UriTemplate
-import org.springframework.hateoas.mediatype.hal.CurieProvider
-import org.springframework.hateoas.mediatype.hal.DefaultCurieProvider
-import org.springframework.web.filter.ForwardedHeaderFilter
 
 
 @SpringBootApplication
-class Application {
-	@Bean
-	fun curieProvider(): CurieProvider? {
-		return DefaultCurieProvider("ra", UriTemplate.of("http://localhost:8080/rels/{rel}"))
-	}
-
-	@Bean
-	fun forwardedHeaderFilter(): ForwardedHeaderFilter? {
-		return ForwardedHeaderFilter()
-	}
-}
+class Application
 
 fun main(args: Array<String>) {
 	runApplication<Application>(*args)
--- src/main/kotlin/de/richargh/sandbox/hypermedia/with/spring/start/web/RootBeans.kt
+++ src/main/kotlin/de/richargh/sandbox/hypermedia/with/spring/start/web/RootBeans.kt
@@ -4,14 +4,36 @@ import de.richargh.sandbox.hypermedia.with.spring.features.account.domain.Accoun
 import de.richargh.sandbox.hypermedia.with.spring.features.account.domain.internal.InMemoryAccounts
 import de.richargh.sandbox.hypermedia.with.spring.features.owners.domain.OwnerFacade
 import de.richargh.sandbox.hypermedia.with.spring.features.owners.domain.internal.InMemoryOwners
+import org.springframework.context.support.BeanDefinitionDsl
 import org.springframework.context.support.beans
+import org.springframework.hateoas.UriTemplate
+import org.springframework.hateoas.mediatype.hal.CurieProvider
+import org.springframework.hateoas.mediatype.hal.DefaultCurieProvider
+import org.springframework.web.filter.ForwardedHeaderFilter
 
 fun rootBeans() = beans {
     bean<StartupListener>()
 
+    webBeans()
+
+    accountBeans()
+
+    ownerBeans()
+}
+
+private fun BeanDefinitionDsl.webBeans() {
+    bean<CurieProvider> {
+        DefaultCurieProvider("ra", UriTemplate.of("http://localhost:8080/rels/{rel}"))
+    }
+    bean<ForwardedHeaderFilter>()
+}
+
+private fun BeanDefinitionDsl.accountBeans() {
     bean<AccountFacade>()
     bean<InMemoryAccounts>()
+}
 
+private fun BeanDefinitionDsl.ownerBeans() {
     bean<OwnerFacade>()
     bean<InMemoryOwners>()
 }
